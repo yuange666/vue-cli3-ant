d1d3048743d7c0e42df5b95313024572fb5ade23
lizhiyuan <zhiyuan.li@bigdatacd.com>
2020-10-10 10:15:59 +0800

    去除多余的代码

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,6 @@
 <template>
         <a-config-provider :autoInsertSpaceInButton="false" :locale="locale">
             <div id="app">
-                <div id="nav">
-                    <router-link to="/">Home</router-link>
-                    |
-                    <router-link to="/about">About</router-link>
-                    <a-tabs default-active-key="2">
-                        <a-tab-pane key="1">
-      <span slot="tab">
-        <img src="./assets/logo.png" alt="">
-        Tab 1
-      </span>
-                            Tab 1
-                        </a-tab-pane>
-                        <a-tab-pane key="2">
-      <span slot="tab">
-        <a-icon type="android" />
-        Tab 2
-      </span>
-                            Tab 2
-                        </a-tab-pane>
-                    </a-tabs>
-                </div>
                 <router-view/>
             </div>
         </a-config-provider>
@@ -38,24 +17,5 @@
     }
 </script>
 <style lang="scss">
-    #app {
-        font-family: 'Avenir', Helvetica, Arial, sans-serif;
-        -webkit-font-smoothing: antialiased;
-        -moz-osx-font-smoothing: grayscale;
-        text-align: center;
-        color: #2c3e50;
-    }
 
-    #nav {
-        padding: 30px;
-
-        a {
-            font-weight: bold;
-            color: #2c3e50;
-
-            &.router-link-exact-active {
-                color: #42b983;
-            }
-        }
-    }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,15 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="固定污染源管理平台新项目"/>
+
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'home',
   components: {
-    HelloWorld
+
   }
 }
 </script>
