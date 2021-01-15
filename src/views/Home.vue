7d5873a1b28c1e3e08dd9f3c07ee82f90199b2c8
lizhiyuan <zhiyuan.li@bigdatacd.com>
2021-01-15 15:23:15 +0800

    自动全局注册基础组件

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,7 @@
 <template>
-    <BaseCard>我是全局注册的基础组件,无需import和注册，直接可以使用</BaseCard>
+    <BaseCard>
+        我是全局自动注册的基础组件,组件命名前缀为Base，组件文件需放置components/base文件夹下
+    </BaseCard>
 </template>
 <script>
     export default {
