7455562915cc00ebdf46308ff5da0ab57dfa528b
lizhiyuan <zhiyuan.li@bigdatacd.com>
2021-07-23 14:23:45 +0800

    图片预加载方法

--- src/plugins/imgPreloader.js
+++ src/plugins/imgPreloader.js
@@ -1,4 +1,4 @@
-//图片预加载
+//图片预加载函数，参数：imgs的数据结构为['xxx.com/p1.png','xxx.com/p1.png'];数组元素为图片的加载地址
 const imgPreloader = url => {
 	return new Promise((resolve, reject) => {
 		let image = new Image();
